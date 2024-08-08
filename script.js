0105461e1bc3b7ff13672eef67cf82435c45a9f9
brenowho <jobs.brenowho@gmail.com>
2024-08-08 16:43:24 -0300

    alteração data limite

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hora = document.getElementById('hora')
 const minuto = document.getElementById('minuto')
 const segundo = document.getElementById('segundo')
 
-const lancamento = "20 oct 2024"
+const lancamento = "01 dec 2024"
 
 function countDown(){
     const dataLancamento = new Date(lancamento)
